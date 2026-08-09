110324cf7771cd641b1219cf9e10d4739926972a
Ankit-IITR <ankityadav08022008@gmail.com>
2026-08-10 01:32:13 +0530

    Revert replacing duplicate image in gallery

--- src/data/pastEvents.ts
+++ src/data/pastEvents.ts
@@ -1,6 +1,6 @@
 import tiranga1 from '@/assets/events/tiranga/DSC_2482.JPG';
 import tiranga3 from '@/assets/events/tiranga/DSC_2511.JPG';
-import tiranga4 from '@/assets/events/tiranga/DSC_2493.JPG';
+import tiranga4 from '@/assets/events/tiranga/DSC_2563.JPG';
 import tiranga5 from '@/assets/events/tiranga/DSC_2590.JPG';
 import tiranga6 from '@/assets/events/tiranga/DSC_2594.JPG';
 import tiranga7 from '@/assets/events/tiranga/DSC_2603.JPG';
